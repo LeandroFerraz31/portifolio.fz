74c82fd2831aa8bb2e759da09ac923deca096956
Leandro ferraz <leandro.santos@universo.univates.br>
2025-07-13 20:06:03 -0300

    Add contacts.ts data file

---
+++ src/data/contacts.ts
@@ -0,0 +1,7 @@
+export const contacts = {
+	whatsapp: "5551999999999",
+	email: "leandro@exemplo.com",
+	linkedin: "https://www.linkedin.com/in/leandro-ferraz-santos/",
+	instagram: "https://www.instagram.com/leandroferraz.of/",
+	github: "https://github.com/LeandroFerraz31",
+};
