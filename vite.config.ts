024d1a93be3cbcd938e0e4d73724eb5d80ca122e
Leandro ferraz <leandro.santos@universo.univates.br>
2025-07-14 16:42:08 -0300

    refactore ajuste de images criando pasta type para ajuste de images!

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), ],
+  plugins: [react()],
+  assetsInclude: ['**/*.png', '**/*.jpeg', '**/*.jpg'],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]', // Preserva os nomes originais
+      },
+    },
+  },
 });
